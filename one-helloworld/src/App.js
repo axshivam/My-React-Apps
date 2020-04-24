88062a15a32b03db2a6568cf076bd425d7d81269
Shiv Sharma <shivsharmavictory@gmail.com>
2020-04-24 10:54:31 -0400

    Hello-World App Complete

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./style.css"
+
+import Button from "./Button"
+
+
+
+
+
+const App = () =>
+{
+  return (
+    <div>
+    <h1 style={{textAlign:"center"}}>Hello World !!!</h1>
+    <Button title="App Store"/>
+    <Button title="Play Store"/>
+    <Button/>
+    </div>
+  );
+};
+
+export default App;
